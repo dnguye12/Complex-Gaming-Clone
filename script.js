6408cb485db4f4636076aada933191f5725302f8
duc-huy.nguyen <duc-huy.nguyen@universite-paris-saclay.fr>
2022-08-31 21:37:09 +0200

    extra javascript and css fixes

---
+++ script.js
@@ -0,0 +1,23 @@
+let tkt = document.getElementsByClassName("tkt")[0];
+
+tkt.addEventListener("click", function() {
+    window.open("https://www.mc-complex.com/forums/tekkit/", "_blank").focus();
+});
+
+let pixel = document.getElementsByClassName("pixel")[0];
+
+pixel.addEventListener("click", function() {
+    window.open("https://www.mc-complex.com/forums/pixelmon/", "_blank").focus();
+});
+
+let ftb = document.getElementsByClassName("ftb")[0];
+
+ftb.addEventListener("click", function() {
+    window.open("https://www.mc-complex.com/forums/ftb/", "_blank").focus();
+});
+
+let van = document.getElementsByClassName("van")[0];
+
+van.addEventListener("click", function() {
+    window.open("https://www.mc-complex.com/forums/vanilla/", "_blank").focus();
+});
